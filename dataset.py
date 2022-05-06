a84c5a6016097f139c21ce79b7a87ad40245a78e
sunxin <sunxin@tetras.ai>
2022-05-06 10:14:52 +0800

    update

--- dataset.py
+++ dataset.py
@@ -113,7 +113,7 @@ class AutoencoderDataset(Dataset):
         data = torch.tensor(self.data[i], dtype=torch.float32).unsqueeze(0)  # require shape [1 256]
         return data
 
-def extraction(data_dir, save_dir, radius, extract_channels, extract_labels, max_idx):
+def extraction_mitbih(data_dir, save_dir, radius, extract_channels, extract_labels, max_idx):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -183,42 +183,15 @@ def extraction(data_dir, save_dir, radius, extract_channels, extract_labels, max
         # break
     savedata = np.array(savedata)
     savelabels = np.array(savelabels)
-    print('all', savedata.shape)
-    print(Counter(savelabels))
-    pd.DataFrame(savedata).to_csv(save_dir + '/data.csv', index=False, header=False)
-    pd.DataFrame(savelabels).to_csv(save_dir + '/labels.csv', index=False, header=False)
+    print('finish all', savedata.shape, Counter(savelabels))
+    pd.DataFrame(savedata).to_csv(save_dir + '/data_mitbih.csv', index=False, header=False)
+    pd.DataFrame(savelabels).to_csv(save_dir + '/labels_mitbih.csv', index=False, header=False)
 
 
-if __name__ == '__main__':
+def extraction_holter(data_dir, save_dir, radius, extract_channels, extract_labels, max_idx):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
-# 2.实现7分类或5分类（七种分类
-# （N：正常L：左束分支阻滞节拍R：右束分支阻滞节拍V：心室过早收缩A：心房性早缩|：I溶质 QRS 样伪影B：左或右束分支块）
-# 3.CNN+LSMT和cnn做对比
-# 4.给一段信号可以实现分类（最好实现可视化
-
-    # extract_channels = [0, 1]
-    # radius = 128
-    # max_idx = 650000
-    # # max_idx = 360 * 10
-    # extract_labels = ['N', 'L', 'R', 'V', 'A', '|']
-    # data_dir = './mit-bih-arrhythmia-database-1.0.0'
-    # save_dir = './data'
-    # extraction(data_dir, save_dir, radius, extract_channels, extract_labels, max_idx)
-
-
-    # # check the dataset 
-    # train_dataset = AutoencoderDataset('./data/normal_test.csv')
-    # for i in train_dataset:
-    #     print(i.shape)
-    #     break
-    # train_dataloader = DataLoader(train_dataset, batch_size=1)
-    # for i, batch_data in enumerate(train_dataloader):
-    #     print(i, batch_data.shape)
-
-
-    data_dir = './european-st-t-database-1.0.0'
-
-    # data_dir = './sudden-cardiac-death-holter-database-1.0.0'
     filenames = [i[:-4] for i in os.listdir(data_dir) if '.atr' in i]
     # print(filenames)
 
@@ -227,42 +200,166 @@ if __name__ == '__main__':
         anno = wfdb.rdann(data_dir + '/' + name, 'atr')
         labels.extend((anno.symbol))
     labels = Counter(labels)
-    print(labels)
 
-    # for lab in extract_labels:
-    #     if lab not in labels:
-    #         print('Error! Not found label:', lab)
+    for lab in extract_labels:
+        if lab not in labels:
+            print('Error! Not found label:', lab)
 
-    # vals = [i for i in range(len(extract_labels))]
-    # label_dict = dict(zip(extract_labels, vals))
-    # print(label_dict)
+    vals = [i for i in range(len(extract_labels))]
+    label_dict = dict(zip(extract_labels, vals))
+    print(label_dict)
 
-    # # import sys
-    # # sys.exit()
+    # import sys
+    # sys.exit()
 
-    # savedata = []
-    # savelabels = []
-    # for name in filenames:
-    #     # record = wfdb.rdheader(data_dir + '/' + name)
-    #     # display(record.__dict__)
+    savedata = []
+    savelabels = []
+    for name in filenames:
+        # record = wfdb.rdheader(data_dir + '/' + name)
+        # display(record.__dict__)
 
-    #     alldata = wfdb.rdrecord(data_dir + '/' + name).p_signal
-    #     length, channels  = alldata.shape
-    #     print('length', length, 'channels', channels)
+        alldata = wfdb.rdrecord(data_dir + '/' + name).p_signal
+        length, channels  = alldata.shape
+        print('length', length, 'channels', channels)
 
-    #     if max_idx > length:
-    #         max_idx = length
-    #     elif max_idx < radius * 2 + 1:
-    #         max_idx = length
-    #     anno = wfdb.rdann(data_dir + '/' + name, 'atr')
-    #     peaks = anno.sample
+        if max_idx > length:
+            max_idx = length
+        elif max_idx < radius * 2 + 1:
+            max_idx = length
+        anno = wfdb.rdann(data_dir + '/' + name, 'atr')
+        peaks = anno.sample
         
-    #     for peak in peaks:
-    #         start = peak - radius
-    #         end = peak + radius
-    #         if start < 0 or end > max_idx:
-    #             continue
+        for peak in peaks:
+            start = peak - radius
+            end = peak + radius
+            if start < 0 or end > max_idx:
+                continue
 
-    #         for ch in range(channels):
-    #             if ch not in extract_channels:
-    #                 continue
+            for ch in range(channels):
+                if ch not in extract_channels:
+                    continue
+
+                tmpdata = wfdb.rdrecord(data_dir + '/' + name, sampfrom=start, sampto=end, channels=[ch]).p_signal
+                tmpdata = np.array(tmpdata).squeeze()
+                tmplabel = wfdb.rdann(data_dir + '/' + name, 'atr', sampfrom=start, sampto=end).symbol
+                # print(tmpdata.shape, len(tmplabel))
+                
+                if len(tmplabel) == 1:
+                    tmplabel = tmplabel[0]
+                    
+                    if tmplabel in extract_labels:
+                        label_val = label_dict[tmplabel]
+
+                        savedata.append(tmpdata)
+                        savelabels.append(label_val)
+            
+
+        print(name, len(savedata), len(savelabels), Counter(savelabels))
+        # break
+    savedata = np.array(savedata)
+    savelabels = np.array(savelabels)
+    print('finish all', savedata.shape, Counter(savelabels))
+    pd.DataFrame(savedata).to_csv(save_dir + '/data_holter.csv', index=False, header=False)
+    pd.DataFrame(savelabels).to_csv(save_dir + '/labels_holter.csv', index=False, header=False)
+
+
+def extraction_european(data_dir, save_dir, radius, extract_channels, extract_labels, max_idx):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    filenames = [i[:-4] for i in os.listdir(data_dir) if '.atr' in i]
+    # print(filenames)
+
+    labels = []
+    for name in filenames:
+        anno = wfdb.rdann(data_dir + '/' + name, 'atr')
+        labels.extend((anno.symbol))
+    labels = Counter(labels)
+
+    for lab in extract_labels:
+        if lab not in labels:
+            print('Error! Not found label:', lab)
+
+    vals = [i for i in range(len(extract_labels))]
+    label_dict = dict(zip(extract_labels, vals))
+    print(label_dict)
+
+    # import sys
+    # sys.exit()
+
+    savedata = []
+    savelabels = []
+    for name in filenames:
+        # record = wfdb.rdheader(data_dir + '/' + name)
+        # display(record.__dict__)
+
+        alldata = wfdb.rdrecord(data_dir + '/' + name).p_signal
+        length, channels  = alldata.shape
+        print('length', length, 'channels', channels)
+
+        if max_idx > length:
+            max_idx = length
+        elif max_idx < radius * 2 + 1:
+            max_idx = length
+        anno = wfdb.rdann(data_dir + '/' + name, 'atr')
+        peaks = anno.sample
+        
+        for peak in peaks:
+            start = peak - radius
+            end = peak + radius
+            if start < 0 or end > max_idx:
+                continue
+
+            for ch in range(channels):
+                if ch not in extract_channels:
+                    continue
+
+                tmpdata = wfdb.rdrecord(data_dir + '/' + name, sampfrom=start, sampto=end, channels=[ch]).p_signal
+                tmpdata = np.array(tmpdata).squeeze()
+                tmplabel = wfdb.rdann(data_dir + '/' + name, 'atr', sampfrom=start, sampto=end).symbol
+                # print(tmpdata.shape, len(tmplabel))
+                
+                if len(tmplabel) == 1:
+                    tmplabel = tmplabel[0]
+                    
+                    if tmplabel in extract_labels:
+                        label_val = label_dict[tmplabel]
+
+                        savedata.append(tmpdata)
+                        savelabels.append(label_val)
+            
+
+        print(name, len(savedata), len(savelabels), Counter(savelabels))
+        # break
+    savedata = np.array(savedata)
+    savelabels = np.array(savelabels)
+    print('finish all', savedata.shape, Counter(savelabels))
+    pd.DataFrame(savedata).to_csv(save_dir + '/data_european.csv', index=False, header=False)
+    pd.DataFrame(savelabels).to_csv(save_dir + '/labels_european.csv', index=False, header=False)
+
+
+if __name__ == '__main__':
+
+    # extract_channels = [0]
+    # radius = 128
+    # max_idx = 650000
+    # extract_labels = ['N', 'L', 'R', 'V', 'A']
+    # data_dir = './mit-bih-arrhythmia-database-1.0.0'
+    # save_dir = './data'
+    # extraction_mitbih(data_dir, save_dir, radius, extract_channels, extract_labels, max_idx)
+
+    # extract_channels = [0,1]
+    # radius = 128
+    # max_idx = 12580000
+    # extract_labels = ['B']
+    # data_dir = './sudden-cardiac-death-holter-database-1.0.0'
+    # save_dir = './data'
+    # extraction_holter(data_dir, save_dir, radius, extract_channels, extract_labels, max_idx)
+
+    extract_channels = [0, 1]
+    radius = 128
+    max_idx = 1800000
+    extract_labels = ['s', 'T']
+    data_dir = './european-st-t-database-1.0.0'
+    save_dir = './data'
+    extraction_european(data_dir, save_dir, radius, extract_channels, extract_labels, max_idx)
